67c0a15561e60177ef9e182c143e7ff190c171d0
GoblinMr <marci.mocsonoky@gmail.com>
2026-01-10 23:33:59 +0100

    Updates

---
+++ vite.config.js
@@ -0,0 +1,86 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'icons/*.png'],
+            manifest: {
+                name: 'GoonAndGain - Edzéstervező',
+                short_name: 'GoonAndGain',
+                description: 'Személyre szabott edzéstervező alkalmazás',
+                theme_color: '#050505',
+                background_color: '#050505',
+                display: 'standalone',
+                orientation: 'portrait',
+                lang: 'hu',
+                start_url: '/',
+                scope: '/',
+                icons: [
+                    {
+                        src: '/icons/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/icon-512-maskable.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+                categories: ['health', 'fitness', 'lifestyle'],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'gstatic-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+                navigateFallback: 'index.html',
+                navigateFallbackDenylist: [/^\/api/],
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+});
